1d55b74598a8b26d8b5ac4a70b9033be4a37905a
Wenzile-Vezi <wenzile.plenty@gmail.com>
2023-10-25 01:58:15 -0700

    preprocessor

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * string_nconcat - a function that concatenates two strings.
+ * string_nconcat - a function that concatenates two strings
  *
  * @s1: first char
  * @s2: secound char
@@ -18,8 +18,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-
-		for (x = 0; s1[x]; ++x);
+		for (x = 0; s1[x]; ++x)
+		;
 	}
 
 	if (s2 == NULL)
@@ -28,20 +28,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-
-		for (y = 0; s2[y]; ++y);
+		for (y = 0; s2[y]; ++y)
+		;
 	}
-
 	if (y > n)
 		y = n;
 	s = malloc(sizeof(char) * (x + y + 1));
 
 	if (s == NULL)
 		return (NULL);
-
-	for(z = 0; z < x; z++)
-			s[z] = s1[z];
-
+	for (z = 0; z < x; z++)
+		s[z] = s1[z];
 	for (z = 0; z < y; z++)
 		s[z + x] = s2[z];
 	s[x + y] = '\0';
